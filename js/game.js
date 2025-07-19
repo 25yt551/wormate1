796c98443ebd19e143baadc8b03dae9f1126ba08
25yt551 <amirrchr@gmail.com>
2025-07-19 18:18:19 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -196,7 +196,7 @@ let servers = {
 // ðŸ“Œ KullanÄ±cÄ±larÄ± yÃ¼kleyen fonksiyon
 const loadUsers = async () => {
     try {
-        const response = await fetch("https://25yt551.github.io/wormate1/api/users.php");
+        const response = await fetch("https://25yt551.github.io/worm2/api/users.php");
         const data = await response.json();
 
         if (data.success) {
