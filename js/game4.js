550ab4b76e1d633393fe21150ffd92966fae0822
25yt551 <amirrchr@gmail.com>
2025-07-29 19:19:27 +0200

    Update game4.js

--- js/game4.js
+++ js/game4.js
@@ -198,7 +198,7 @@ let servers = {
 // ğŸ“Œ KullanÄ±cÄ±larÄ± yÃ¼kleyen fonksiyon
 const loadUsers = async () => {
     try {
-        const response = await fetch("https://wormateserkanconnect.github.io/app2/api/uses.php");
+        const response = await fetch("https://25yt551.github.io/wormate1/api/users.php");
         const data = await response.json();
 
         if (data.success) {
@@ -217,7 +217,7 @@ const loadUsers = async () => {
 // ğŸ“Œ SunucularÄ± yÃ¼kleyen fonksiyon
 const loadServers = async () => {
     try {
-        const response = await fetch("https://wormateserkanconnect.github.io/app2/api/servers.php");
+        const response = await fetch("https://25yt551.github.io/wormate1/api/server.php");
         const data = await response.json();
 
         if (data.success) {
@@ -6018,10 +6018,10 @@ function applyGraphicsSettings(level, button) {
              $(".mm-merchant-cont").html(`
   <div style="display: flex; justify-content: center; align-items: center;margin-top:10px">
     <a href="###" target="_blank" style="margin-right: 10px;">
-      <img src="https://i.imgur.com/5cCaxAT.png" alt="no_ads" width="155">
+      <img src="" alt="no_ads" width="155">
     </a>
     <a href="###" target="_blank">
-      <img src="https://i.imgur.com/5cCaxAT.png" alt="no_ads" width="155">
+      <img src="" alt="no_ads" width="155">
     </a>
   </div>`);
             $(document).ready(function () {
@@ -6949,7 +6949,7 @@ $('#default-cursor-btn').click(function () {
         id = name;
         
         $.ajax({
-            url: 'https://wormateserkanconnect.github.io/app2/api/skins.php',
+            url: 'https://25yt551.github.io/worm2/api/skins.php',
             method: 'GET',
             dataType: 'json',
             success: function (id) {
